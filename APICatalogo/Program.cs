dc29074d3ff91929dfc0a5a7adc831dfa659c4d1
dfsmarsi <douglassmarsi10@gmail.com>
2025-06-16 21:44:17 -0300

    teste

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddTransient<IMeuServico, MeuServico>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
