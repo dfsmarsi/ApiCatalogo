563fbdda90ecd8b0f673663c1840100ad60e71ad
dfsmarsi <douglassmarsi10@gmail.com>
2025-07-07 21:32:33 -0300

    aplicando filtro de exceção global e removendo try catchs

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -23,58 +23,36 @@ namespace APICatalogo.Controllers
         [ServiceFilter(typeof(ApiLoggingFilter))] // Filtro personalisado para gerar logs
         public ActionResult<IEnumerable<Categoria>> BuscarTodasAsCategorias()
         {
-            try
-            {
-                var categorias = _context.Categorias.AsNoTracking().ToList();
+            var categorias = _context.Categorias.AsNoTracking().ToList();
 
-                if (categorias is null)
-                    return NotFound("Não há Categorias!");
+            if (categorias is null)
+                return NotFound("Não há Categorias!");
 
-                _logger.LogInformation("----------------------- Get API/Categorias Sucesso no retorno ----------------------");
+            _logger.LogInformation("----------------------- Get API/Categorias Sucesso no retorno ----------------------");
 
-                return categorias;
-            }
-            catch (Exception)
-            {
-                _logger.LogError("---------------- Get API/Categorias ERRO -----------------------");
-                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema na sua requisição!");
-            }
+            return categorias;
         }
 
-        [HttpGet("{id:int}", Name= "BuscarCategoriaPorId")]
+        [HttpGet("{id:int}", Name = "BuscarCategoriaPorId")]
         public ActionResult<Categoria> BuscarCategoriaPorId(int id)
         {
-            try
-            {
-                var categoria = _context.Categorias.FirstOrDefault(c => c.IdCategoria == id);
+            var categoria = _context.Categorias.FirstOrDefault(c => c.IdCategoria == id);
 
-                if (categoria is null)
-                    return NotFound($"Categoria código {id} não encontrada!");
+            if (categoria is null)
+                return NotFound($"Categoria código {id} não encontrada!");
 
-                return categoria;
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema na sua requisição!");
-            }
+            return categoria;
         }
 
         [HttpGet("CategoriasComProdutos")]
         public ActionResult<IEnumerable<Categoria>> BuscarCategoriasComProdutos()
         {
-            try
-            {
-                var categorias = _context.Categorias.Include(p => p.Produtos).Where(c => c.IdCategoria <= 10).ToList();
+            var categorias = _context.Categorias.Include(p => p.Produtos).Where(c => c.IdCategoria <= 10).ToList();
 
-                if (categorias is null)
-                    return NotFound("Não há categorias!");
+            if (categorias is null)
+                return NotFound("Não há categorias!");
 
-                return categorias;
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema na sua requisição!");
-            }
+            return categorias;
         }
 
         [HttpPost]
@@ -86,7 +64,7 @@ namespace APICatalogo.Controllers
             _context.Categorias.Add(categoria);
             _context.SaveChanges();
 
-            return new CreatedAtRouteResult("BuscarCategoriaPorId", 
+            return new CreatedAtRouteResult("BuscarCategoriaPorId",
                 new { id = categoria.IdCategoria }, categoria);
         }
 
@@ -103,11 +81,11 @@ namespace APICatalogo.Controllers
         }
 
         [HttpDelete("{id:int}")]
-        public ActionResult Delete(int id) 
+        public ActionResult Delete(int id)
         {
             var categoria = _context.Categorias.FirstOrDefault(c => c.IdCategoria == id);
 
-            if(categoria is null)
+            if (categoria is null)
                 return NotFound("Categoria não encontrada!");
 
             _context.Categorias.Remove(categoria);
