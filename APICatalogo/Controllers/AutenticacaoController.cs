b8191be4501c7e777056dc48fe0202b51a9ed927
dfsmarsi <douglassmarsi10@gmail.com>
2026-01-20 22:20:05 -0300

    pequenas implementacoes token jwt

---
+++ APICatalogo/Controllers/AutenticacaoController.cs
@@ -0,0 +1,36 @@
+﻿using APICatalogo.Models;
+using APICatalogo.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class AutenticacaoController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AutenticacaoController (
+            ITokenService tokenService,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        //[HttpPost]
+        //[Route ("login")]
+        //public async Task<IActionResult> Login([FromBody] LoginModel model)
+        //{
+
+        //}
+    }
+}
